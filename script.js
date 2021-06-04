f12abe0fdf555f0ef0afba5987f024c6a143769f
TonAlmeida <eltonsantos.almeida@outlook.com>
2021-06-04 02:40:45 -0300

    setting the buttons

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector(`[data-new-list-form]`);
 const newListInput = document.querySelector(`[data-new-list-input]`);
 const delteListButton = document.querySelector(`[deta-delete-list-button]`);
-const listDisplayContainer = document.querySelector(`[data-list-display-container]`);
+const listDisplayContainer = document.querySelector(`[data-list-display-container]`);//todo-list
 const listTitleElement = document.querySelector(`[data-list-title]`);
 const listCountElement = document.querySelector(`[data-list-count]`);
 const tasksContainer = document.querySelector(`[data-tasks]`);
 const taskTemplate = document.getElementById(`task-template`);
 const newTaskForm = document.querySelector(`[data-new-task-form]`);
 const newTaskInput = document.querySelector(`[data-new-task-input]`);
+const clearCompleteTasksButton = document.querySelector(`[data-clear-complete-tasks-button]`)
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -20,6 +21,22 @@ listsContainer.addEventListener(`click`, e => {
         selectedListId = e.target.dataset.listId;
         saveAndRender();
     }
+})
+
+tasksContainer.addEventListener(`click`, e => {
+    if(e.target.tagName.toLowerCase() === `li`) {
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
+        selectedTask.complete = e.target.checked;
+        save();
+        renderTaskCount(selectedList);
+    }
+})
+
+clearCompleteTasksButton.addEventListener(`click`, e => {
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
+    saveAndRender();
 })
 
 delteListButton.addEventListener(`click`, e => {//delete button
@@ -49,6 +66,16 @@ newTaskForm.addEventListener(`submit`, e => {
     saveAndRender();
 })
 
+tasksContainer.addEventListener(`click`, e => {
+    if(e.target.tagName.toLowerCase() === `input`) {
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
+        selectedTask.complete = e.target.checked;
+        save();
+        renderTaskCount(selectedList);
+    }
+})
+
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
@@ -60,11 +87,7 @@ function saveAndRender() {
 }
 
 function createList(name) {
-    return {id: Date.now().toString(), name: name, tasks: [{
-        id: `bla`,
-        name: `Test`,
-        complete: true
-    }]}
+    return {id: Date.now().toString(), name: name, tasks: []}
 }
 
 function createTask(name) {
@@ -76,7 +99,7 @@ function render() {
     renderLists();
 
     const selectedList = lists.find(list => list.id === selectedListId);
-    if(selectedListId == null) {
+    if(selectedListId == null || selectedList === undefined) { //I added a undefined option here to avoid show a empty box
         listDisplayContainer.style.display = `none`;
     }else {
         listDisplayContainer.style.display = ``;
